04459b1c5996b44a69778ff7f0e48a83c57abe46
DavidAlarconVargas <daalarconv@unal.edu.co>
2023-05-03 00:14:40 -0500

    new

--- initial-case/pages/_app.js
+++ initial-case/pages/_app.js
@@ -1,5 +1,5 @@
 import 'Mega/styles/globals.css'
-
+//Acá van las cosas globales, barras de menu, diseño etcs, junto con su hoja de estilos globales
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
---
+++ initial-case/pages/contacto.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function paginaContacto() {
+  return (
+    <div>
+      <Head>
+        <title>Página Contacto</title>
+        <meta name="descrition" content='Contacto de clientes potenciales'/>
+      </Head>
+
+      <h2>Página Contacto</h2>
+      <p>Párrafo de Prueba</p>
+      <h4>Subtitulo de Prueba</h4>
+
+      </div>
+  )
+}
