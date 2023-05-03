e13233efbe14d319f72d4d08747e1d6ad7a9230c
DavidAlarconVargas <daalarconv@unal.edu.co>
2023-05-03 13:00:56 -0500

    Actualizao

---
+++ authapp/pages/login.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Layout from 'Mega/layout/layuot'
+export default function login(){
+    return(
+        <Layout>
+
+
+        <Head>
+            <title>Register</title>
+        </Head>
+
+
+
+            <h1>login</h1>
+        </Layout>
+    )
+}
---
+++ authapp/pages/register.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Layout from 'Mega/layout/layuot'
+export default function register(){
+    return(
+        <Layout>
+
+
+        <Head>
+            <title>Login</title>
+        </Head>
+
+
+
+            <h1>register</h1>
+        </Layout>
+    )
+}
+
