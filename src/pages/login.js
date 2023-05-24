d0efab22e743d429fe39b5b073ccb51f8440e498
DavidAlarconVargas <daalarconv@unal.edu.co>
2023-05-23 21:41:22 -0500

    error meseges

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,18 +2,41 @@ import Head from 'next/head'
 import Layout from '/src/pages/login/layout/layuot'
 import Link from 'next/link'
 import styles from '/src/styles/Formlogin.module.css';
+import Image from 'next/image';
 import { HiAtSymbol,HiEyeOff,HiEye } from "react-icons/hi";
 import { useState } from 'react';
 import {signIn, signOut } from "next-auth/react"
-//BUENO ME FALTA LA WEBONADA DE 
+import {useFormik} from 'formik';
+import login_validate from '../lib/validate';
+//BUENO ME FALTA LA WEBONADA DE, se me olvido
 
 export default function login() {
     
     const[show,setshow]= useState(false)
+    //formik hook
+    //https://formik.org/docs/overview
+    const formik = useFormik({
+        initialValues: {
+            email:'',
+            password:''
+            
+        },
+        validate : login_validate,
+        onSubmit
+    })
+
+    async function onSubmit(values){
+        console.log(values)
+    }
+
+
     //Google Handler function
     async function handleGoogleSignin(){
+        //https://next-auth.js.org/
         signIn('google', {callbackUrl:"http://localhost:3000/"})
     }
+
+
     return (
         <Layout>
             <Head>
@@ -31,34 +54,35 @@ export default function login() {
                         inicia sesión
                     </p>
                 </div>
-
                 {/* form */}
-
                 {/* Space text */}
-                <form className="flex flex-col gap-5">
+                <form className="flex flex-col gap-5" onSubmit={formik.handleSubmit}>
                     <div className={styles.input_group}>
                         <input 
                         type="email"
                         name="email"
                         placeholder="Correo electrónico"
                         className={styles.input_text}
+                        {...formik.getFieldProps('email')}
                         />
                         <span className='icon flex items-center px-4'>
                             <HiAtSymbol size={25} />
                         </span>
                     </div>
+                    {formik.errors.email && formik.touched.email ? <span className='text-rose-500'>{formik.errors.email}</span>:<></>}
                     <div className={styles.input_group}>
                         <input 
                             type={`${show ? "text": "password"}`} 
                             name="password"
                             placeholder="Contraseña"
                             className={styles.input_text}
+                            {...formik.getFieldProps('password')}
                         />
                         <span className='icon flex items-center px-4' onClick={() => setshow(!show)}>
                             {show ? <HiEye size={25} /> : <HiEyeOff size={25} />}
                         </span> 
                     </div>
-                    
+                    {formik.errors.password && formik.touched.password ? <span className='text-rose-500'>{formik.errors.password}</span>:<></>}
                     {/* Bottom Iniciar Sessión */}
                     <div className="input-buttom">
                         <button type='submit' className={styles.button}>
